2e432bd41147f165a1a7bb9b0bd6bb5d771725a4
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-01-15 18:01:48 +0000

    project setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-
+    <Switch>
+      <Route path='/'>
+        <Home />
+      </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
