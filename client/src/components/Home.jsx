dc2c18bd03122240ba718218a9ebbaad78b14e5e
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-16 03:06:34 +0000

    added background img

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,12 @@
 export default function Home(props) {
   return (
-    <div className="header">
-      <div className="header-content">
-        <h1 id="title-animation" className="h1">Support local stores in your neigborhood</h1>
-        <p id="subtitle-animation" className="paragraph"><strong className="bold-text">Enter your postal code</strong></p>
-        <p className="paragraph-2">Get your fresh local products basket in couple clicks</p>
+      <div className="header" style={{ backgroundImage: "url(/veggies.jpeg)" }}>
+        <div className="header-content">
+          <h1 id="title-animation" className="h1">Support local stores in your neigborhood</h1>
+          <p id="subtitle-animation" className="paragraph"><strong className="bold-text">Enter your postal code</strong></p>
+          <p className="paragraph-2">Get your fresh local products basket in couple clicks</p>
+        </div>
       </div>
-    </div>
   )
 }
 
