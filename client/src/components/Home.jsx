b7cbd0cc69edc9ad606188ba7de44e0fd0ab6c95
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-16 18:31:19 +0000

    modify input/form

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,31 @@
+import React, { useState } from 'react';
+
 export default function Home(props) {
+  const [zipcode, setZipcode] = React.useState("");
+
+
   return (
       <div className="header-image" style={{ backgroundImage: "url(/veggies.jpeg)" }}>
         <div className="header-content">
           <h1 id="title-animation" className="h1">Support local stores in your neigborhood</h1>
-          <p id="subtitle-animation" className="paragraph">Enter your postal code to start shopping</p>
-          <input class="postal" placeholder="eg. H1X 4F5"></input>
+          <p id="subtitle-animation" className="paragraph">Enter your postal code</p>
+          <form class="postal">
+            <input 
+              id="zip" 
+              name="zip" 
+              type="text" 
+              placeholder="eg. H1X 4F5" 
+              onChange={(event) => { 
+                const { value } = event.target;
+                setZipcode(value.replace("[A-Za-z][0-9][A-Za-z] [0-9][A-Za-z][0-9]").substr(0, 6))
+              }}
+            />
+            <button 
+              type="submit" 
+              class="submit-button"
+              // onClick={setZipcode}
+              >Submit</button>
+          </form>
           <p className="paragraph-2">Get your fresh local products basket in couple clicks</p>
         </div>
       </div>
