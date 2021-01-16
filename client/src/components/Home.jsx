fd86657913fdcf9232437d79607e4197f512f494
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-16 16:09:23 +0000

    added input and font

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,10 @@
 export default function Home(props) {
   return (
-      <div className="header" style={{ backgroundImage: "url(/veggies.jpeg)" }}>
+      <div className="header-image" style={{ backgroundImage: "url(/veggies.jpeg)" }}>
         <div className="header-content">
           <h1 id="title-animation" className="h1">Support local stores in your neigborhood</h1>
-          <p id="subtitle-animation" className="paragraph"><strong className="bold-text">Enter your postal code</strong></p>
+          <p id="subtitle-animation" className="paragraph">Enter your postal code to start shopping</p>
+          <input class="postal" placeholder="eg. H1X 4F5"></input>
           <p className="paragraph-2">Get your fresh local products basket in couple clicks</p>
         </div>
       </div>
