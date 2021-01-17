41259a246be686a4c544ce0b24b9f81985f7f648
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-17 21:40:08 +0000

    added product component

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -2,22 +2,23 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 export default function ProductList() {
-  const [products, setProducts] = useState([])
+  const [product, setProductData] = useState([])
 
-  const url = 'http://localhost:3001/api/items'
 
   useEffect(() => {
     getAllProducts();
   })
 
-  const getAllProducts = () => {
-    axios.get(url)
-    .then((response) => {
-      const allProducts = response.data.products.allProducts;
-      setItems(allProducts)
-    })
-    .catch(error => console.error(`Error: ${error}`))
-  }
+  function getAllProducts() {
+    axios.get('https://localhost:3001/api/products')
+         .then(response => {
+            console.log(response.data)
+            const { data } = response.data
+            SetProductData(data)
+        }).catch(error => {
+            console.log(error);
+        })
+    }
   return (
     <></>
   )
