c77e2aef507384d3bff0890efd605b193a1942e8
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-17 02:21:34 +0000

    modif logo and menu

--- client/src/components/NavMenu.jsx
+++ client/src/components/NavMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './NavMenu.css'
 import {
   BrowserRouter as Router,
   Switch,
@@ -28,17 +28,20 @@ export default function NavMenu() {
         Menu
         </Button>
       <Menu
+        className="menu"
         id="simple-menu"
         anchorEl={anchorEl}
         keepMounted
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem><Link to="/" onClick={handleClose}>Home</Link></MenuItem>
-        <MenuItem><Link to="/login" onClick={handleClose}>Login</Link></MenuItem>
-        <MenuItem><Link to="/register" onClick={handleClose}>Register</Link></MenuItem>
-        <MenuItem><Link to="/checkout" onClick={handleClose}>Checkout Cart</Link></MenuItem>
-        <MenuItem><Link to="/logout" onClick={handleClose}>Logout</Link></MenuItem>
+        <div className="menu-item">
+          <MenuItem><Link to="/" onClick={handleClose}>Home</Link></MenuItem>
+          <MenuItem><Link to="/login" onClick={handleClose}>Login</Link></MenuItem>
+          <MenuItem><Link to="/register" onClick={handleClose}>Register</Link></MenuItem>
+          <MenuItem><Link to="/checkout" onClick={handleClose}>Checkout Cart</Link></MenuItem>
+          <MenuItem><Link to="/logout" onClick={handleClose}>Logout</Link></MenuItem>
+        </div>
       </Menu>
     </div>
   );
