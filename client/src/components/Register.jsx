4ea398a590fd68f3a48e77d11b909720c8468480
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-18 19:12:14 +0000

    improved register

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import NavMenu from './NavMenu'
+import NavMenu from './NavMenu';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Register(props) {
   const [state, setState] = useState({
@@ -13,31 +15,66 @@ export default function Register(props) {
   }) 
 
   const [error, setError] = useState("");
+  const history = useHistory();
 
-  // Update user state based on their input
+  // hook used to handle values entered by the user
   const handleChange = (e) => {
-    console.log(e.target.value)
     const { id, value } = e.target
-    setState(prev => ({
-      ...prev, 
+    console.log(e.target.value)
+    setState(prevState => ({
+      ...prevState, 
       [id] : value
     }))
   }
 
-
- const onSubmit = (e) => {
-   e.preventDefault();
-   const userData = {
-     "firstName": state.firstName,
-     "lastName": state.lastName,
-     "email": state.email,
-     "password": state.password,
-     "phoneNumber": state.phoneNumber,
-     "address": state.address,
-     "city": state.city
-   }
+  const userData = {
+    "firstName": state.firstName,
+    "lastName": state.lastName,
+    "email": state.email,
+    "password": state.password,
+    "phoneNumber": state.phoneNumber,
+    "address": state.address,
+    "city": state.city
   }
 
+  // function to make a backend API request
+  const sendDetailsToServer = () => {
+    if (!userData.firstName || !userData.lastName || !userData.email || !userData.password || !userData.address || !userData.phoneNumber || !userData.city ) {
+        setError("Please fill all the forms")
+    } else {
+      axios.post('http://localhost:3001/api/users/register', userData)
+      .then(function (response) {
+        if(response.status === 200){
+          setState(prevState => ({
+            ...prevState,
+            'successMessage' : 'Registration successful. Redirecting to Home page...'
+          }))
+          setError(null)
+          redirectToHome();
+        } else {
+          setError('Some error occured')
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    }
+  }
+
+  // click event handler for sending request to backend 
+  const handleSubmitClick = (e) => {
+    e.preventDefault();
+    if(userData) {
+        sendDetailsToServer()    
+    } else {
+        props.showError('Passwords do not match');
+    }
+  }
+
+
+  const redirectToHome = () => {
+    history.push('/home')
+  }
 
 
   return (
@@ -57,6 +94,7 @@ export default function Register(props) {
         <span>
           <label for="firstName"></label>
           <input 
+            id="first-name"
             name="firstName" 
             type="text"  
             placeholder="First Name"
@@ -68,6 +106,7 @@ export default function Register(props) {
         <span>
           <label for="lastName"></label>
           <input 
+            id="last-name"
             name="lastName" 
             type="text"  
             placeholder="Last Name"
@@ -79,6 +118,7 @@ export default function Register(props) {
         <span>
           <label for="email"></label>
           <input 
+            id="email"
             name="email" 
             type="text"  
             placeholder="Email"
@@ -90,8 +130,9 @@ export default function Register(props) {
         <span>
           <label for="password"></label>
           <input 
+            id="password"
             name="password" 
-            type="text"  
+            type="password"  
             placeholder="Password"
             value={state.password}
             onChange={handleChange}
@@ -101,6 +142,7 @@ export default function Register(props) {
         <span>
           <label for="address"></label>
           <input 
+            id="address"
             name="address" 
             type="text"  
             placeholder="Address"
@@ -112,6 +154,7 @@ export default function Register(props) {
         <span>
           <label for="city"></label>
           <input 
+            id="city"
             name="city" 
             type="text"  
             placeholder="City"
@@ -123,6 +166,7 @@ export default function Register(props) {
         <span>
           <label for="phoneNumber"></label>
           <input 
+            id="phone-number"
             name="phoneNumber" 
             type="number"  
             placeholder="Phone Number"
@@ -132,9 +176,13 @@ export default function Register(props) {
           />
         </span>
         <div className="button-register">
-          <button type="submit" class="submit-button" onClick={onSubmit}>Submit</button>
+          <button type="submit" class="submit-button" onClick={handleSubmitClick}>Submit</button>
         </div>
       </form>
+      <div className="questionLogin">
+      <span className="login-register">Already have an account?</span>
+      <span><a id="login-reg" href="/login">Please Login</a></span>
+      </div>
     </div>
   </div>
     )
