268c61ad4679dce0a1d3ea64c847f01eb1862cd3
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-01-20 14:41:00 -0500

    register functionality fully implemented

--- backend-express/helpers/dbhelpers.js
+++ backend-express/helpers/dbhelpers.js
@@ -23,7 +23,7 @@ module.exports = (db) => {
             .catch((err) => err);
     }
   
-    const addUser = (firstName, lastName, email, password) => {
+    const addUser = (firstName, lastName, email, password, phone_number, address, city) => {
         const query = {
             text: `INSERT INTO users (first_name, last_name, email, password, phone_number, address, city) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *` ,
             values: [firstName, lastName, email, password, phone_number, address, city]
