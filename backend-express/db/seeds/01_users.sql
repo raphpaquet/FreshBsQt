89a58401462bc680b27064d494c7c42167e951e8
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-01-20 14:35:36 -0500

    changed double quotes to single quotes in users seeds

--- backend-express/db/seeds/01_users.sql
+++ backend-express/db/seeds/01_users.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (first_name, last_name, email, password, phone_number, address, city)
-VALUES ('Mario', 'Bros', 'itsAme@mail.com', password, '514-222-2222', '222 Mushroom Road', 'M-kingdom')
+VALUES ('Mario', 'Bros', 'itsAme@mail.com', 'password', '514-222-2222', '222 Mushroom Road', 'M-kingdom')
