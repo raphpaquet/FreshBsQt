330886151829d4b4be522862c351de367f7d2253
vagrant <matt.hd.ford@gmail.com>
2021-01-20 23:07:41 +0000

    saving for merge

--- client/src/components/ItemDropDown.js
+++ client/src/components/ItemDropDown.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import axios from 'axios';
 import Button from '@material-ui/core/Button';
@@ -69,29 +69,44 @@ export default function ItemDropDown () {
   const [showDesserts, setShowDesserts] = useState(false);
   const [showOther, setShowOther] = useState(false);
   // For the bottom drawer that holds the items
-  const classes = useStyles();
-  const [state, setState] = React.useState({
+  const [drawerState, setDrawerState] = useState({
     bottom: false,
   });
-
-  axios.get(`http://localhost:3001/api/products`)
-    .then(res => {
-      const products = res.data;
-      console.log(products);
-    })
-    .catch(error => {
-      console.log(error)
-    });
+  // The products from the axios call
+  const [productsData, setProductsData] = useState('');
 
 
 
 
+  const classes = useStyles();
+
+  // Axios call to get the product list from the backend
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+
+  const getProducts = () => {
+    axios.get(`http://localhost:3001/api/products`)
+      .then((response) => {
+        const products = response.data
+        setProductsData(products);
+        console.log(products)
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }
+
+
+
+  // Handler for the products list that opens and closes on page. 
   const toggleDrawer = (anchor, open) => (event) => {
     if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
     }
 
-    setState({ ...state, [anchor]: open });
+    setDrawerState({ ...drawerState, [anchor]: open });
   };
 
 
@@ -227,18 +242,7 @@ export default function ItemDropDown () {
                 <div className="product-image-section">
                   <img src="./images/citrus.jpeg" alt="citrus" />
                 </div>
-                <h3>Product Title</h3>
-                <h5>From Store Name</h5>
-                <div className="price-and-add">
-                  <span>$1.99</span><button>Add</button>
-                </div>
-              </div>
-
-              <div className="product-wrapper">
-                <div className="product-image-section">
-                  <img src="./images/citrus.jpeg" alt="citrus" />
-                </div>
-                <h3>Product Title</h3>
+                <h3>{productsData[3].name}</h3>
                 <h5>From Store Name</h5>
                 <div className="price-and-add">
                   <span>$1.99</span><button>Add</button>
@@ -330,7 +334,7 @@ export default function ItemDropDown () {
             <SwipeableDrawer
               anchor={anchor}
               transitionDuration={1100}
-              open={state[anchor]}
+              open={drawerState[anchor]}
               onClose={toggleDrawer(anchor, false)}
               onOpen={toggleDrawer(anchor, true)}
             >
