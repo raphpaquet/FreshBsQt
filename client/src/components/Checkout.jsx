15a54173bcd227693d81b14aa2f2de1245bcc407
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-01-23 17:31:09 +0000

    passed user down to checkout form

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { StripeProvider, Elements } from 'react-stripe-elements'
 import CheckoutForm from './CheckoutForm'
 
-const Checkout = ({ selectedProduct, history }) => {
+const Checkout = ({ selectedProduct, history, user }) => {
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
@@ -10,7 +10,7 @@ const Checkout = ({ selectedProduct, history }) => {
   return (
     <StripeProvider apiKey="pk_test_51I6Jt9HsmgbtTrbqfCLFE3iOym9EzX83fUD26QzcT23sPMbAdp0lCcj0gev2ldujHpqI7HbTMa3unA5acP6UaN3U00AcA7HfMG">
       <Elements>
-        <CheckoutForm selectedProduct={selectedProduct} history={history} />
+        <CheckoutForm user = {user} selectedProduct={selectedProduct} history={history} />
       </Elements>
     </StripeProvider>
   )
