0fa323eceff7c334e4d55f036af8ace812234a0d
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-01-23 01:00:10 -0500

    added seeds products

--- backend-express/db/seeds/02_products.sql
+++ backend-express/db/seeds/02_products.sql
@@ -29,3 +29,52 @@ INSERT INTO products (name, description, quantity, image, price, category, store
 VALUES ('Pineapple', 'pineapple', 7, './public/docs/pineapple.jpeg', 5.99, 'fruits', 5);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Grapefruit', 'grapefruit', 7, './public/docs/grapefruit.jpeg', 2.99, 'fruits', 5);
+
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Bâtard campagne ', 'Bâtard shaped loaf on a base of rye flour', 10, './public/docs/batard.jpeg', 4.50, 'bread', 5);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Fresh Montreal Bagels', 'Our classic sesame bagels are rolled in sesame seeds', 30, './public/docs/bagels.jpg', 1.75, 'bread', 4);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Fresh Montreal Bagels ', 'Also known as “the everything” bagel, a mix of sesame, poppy, garlic, and onion for a burst of bold texture and flavor.', 20, './public/docs/alldressbagel.jpg', 3.75, 'bread', 4);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Fougasse Olives', 'Appetizer bread with kalamata olives on a poolish base with extra-virgin olive oil', 8, './public/docs/fougasse.jpg', 5.25, 'bread', 5);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('baguette', 'Traditional French-style baguette on a poolish base, mono-scarred', 20, './public/docs/baguette.jpg', 3.00, 'bread', 3);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Grano loaf', ' whole wheat loaf on a sourdough & yeast base, loaded with a mix of 9 whole grains', 6, './public/docs/grainbread.jpg', 5.75, 'bread', 3);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Croissant', '100% pure butter flaky leavened pastry dough', 20, './public/docs/croissant.jpg', 1.25, 'bread', 3);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Chocolate croissant', '100% pure butter flaky leavened pastry dough, with chocolate', 20, './public/docs/chocolatine.jpg', 1.35, 'bread', 5);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Almond croissant', 'Butter croissant stuffed with almond cream frangipane and sliced brown almonds', 10, './public/docs/almondcroissand.webp', 3.75, 'bread', 5);
+
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Blueberry pie', 'fresh blueberry pie with butter crust', 7, './public/docs/blueberrypie.jpg', 22.99, 'desserts', 2);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('10" Seasonal pie duo', 'All-butter pie crust. Flavor: Quebec Apple, cherry, sugar pie, pecan or pear ', 4, './public/docs/pieduo.jpg', 49.99, 'desserts', 2);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Lemon tart', 'A sweet dough shell with a baked lemon filling', 7, './public/docs/lemonpie.jpg', 18.99, 'desserts', 5);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Assorted cookies', 'Some favorites include seasonal decorated sugar cookies, shortbread, oatmeal and other varieties', 7, './public/docs/cookie.jpg', 10.99, 'desserts', 2);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Chocolate Cheese cake', 'New York cheesecake from the cheesecake factory plain or dipped in pure chocolate dips.', 5, './public/docs/cheesecake.jpg', 36.99, 'desserts', 3);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Assorted Cupcakes', 'Get a box of 8 fresh assorted flavor cupcakes', 7, './public/docs/cupcake.jpg', 22.99, 'desserts', 3);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Assorted Donuts', 'Get a box of 8 fresh assorted flavor donuts', 8, './public/docs/donuts.jpg', 14.99, 'desserts', 3);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Carrot cake', 'The best carrot cake in town.', 9, './public/docs/carrotcake.jpg', 23.99, 'desserts', 3);
+
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Oka cheese', 'This semi-soft cheese is made with pasteurized milk and features a brine-washed rind', 9, './public/docs/oka.png', 14.99, 'cheese', 2);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Brie Paysan', 'A beautiful creamy cheese, the Brie Paysan is made with whole milk.', 7, './public/docs/brie.jpg', 19.99, 'cheese', 4);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Blue of Elizabeth', 'semi-soft cheese made from thermalized milk with interior mould', 9, './public/docs/bluecheese.jpg', 21.99, 'cheese', 4);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Buffala', 'mozzarella made from the milk of domestic water buffalo', 18, './public/docs/buffala.jpg', 18.99, 'cheese', 2);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Pont-Mousseau', 'This is an unripened soft white cheese. Its silky texture can compare to a mix of a mousse and cream cheese', 10, './public/docs/pontmousseau.jpg', 12.99, 'cheese', 4);
+INSERT INTO products (name, description, quantity, image, price, category, store_id)
+VALUES ('Black horse of the island', 'Mild, with lactic notes, this semi-firm paste cheese is covered with a thin coat of vegetable ash and a bloomy rind.', 6, './public/docs/blackhorse.jpg', 24.99, 'cheese', 4);
