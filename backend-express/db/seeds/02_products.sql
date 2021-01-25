04f47f52a3631a463fbbc9f878d32296aa57c77f
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-01-25 16:37:30 -0500

    added stores

--- backend-express/db/seeds/02_products.sql
+++ backend-express/db/seeds/02_products.sql
@@ -1,34 +1,34 @@
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Radishes', 'Bunch of radishes', 9, './docs/raddish.jpeg', 3.99, 'Vegetables', 3);
+VALUES ('Radishes', 'Bunch of radishes', 9, './docs/raddish.jpeg', 3.99, 'Vegetables', 1);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Asparagus', 'Bunch of asparagus', 10, './docs/asparagus.jpeg', 2.99, 'Vegetables', 2);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Cabbage', 'Individual Cabbage', 8, './docs/cabbage.jpeg', 2.99, 'Vegetables', 2);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Eggplant', 'Individual Eggplant', 8, './docs/eggplant.jpeg', 1.99, 'Vegetables', 3);
+VALUES ('Eggplant', 'Individual Eggplant', 8, './docs/eggplant.jpeg', 1.99, 'Vegetables', 7);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Artichokes', 'Fresh delicious artichokes', 10, './docs/artichoke.jpeg', 3.99, 'Vegetables', 2);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Onions', 'Bag of onions', 12, './docs/onion .jpeg', 3.99, 'Vegetables', 3);
+VALUES ('Onions', 'Bag of onions', 12, './docs/onion .jpeg', 3.99, 'Vegetables', 7);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Pasnip', 'Bunch of parsnips', 12, './docs/parsnip.jpeg', 0.99, 'Vegetables', 3);
+VALUES ('Pasnip', 'Bunch of parsnips', 12, './docs/parsnip.jpeg', 0.99, 'Vegetables', 6);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Tomatoes', 'Box of fresh tomatoes', 14, './docs/tomato.jpeg', 2.99, 'Vegetables', 4);
+VALUES ('Tomatoes', 'Box of fresh tomatoes', 14, './docs/tomato.jpeg', 2.99, 'Vegetables', 7);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Avocado', 'Great on toast', 5, './docs/avocado.jpeg', 3.99, 'Vegetables', 4);
+VALUES ('Avocado', 'Great on toast', 5, './docs/avocado.jpeg', 3.99, 'Vegetables', 2);
 
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Blueberries', 'Box of Blueberries', 20, './docs/blueberry.jpeg', 3.99, 'fruits', 1);
+VALUES ('Blueberries', 'Box of Blueberries', 20, './docs/blueberry.jpeg', 3.99, 'fruits', 7);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Raspberries', 'Box of raspberries', 20, './docs/raspberry.jpeg', 3.99, 'fruits', 1);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Apples', 'Bag of apples', 7, './docs/apple.jpeg', 4.99, 'fruits', 1);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Strawberries', 'Fresh strawberries', 7, './docs/strawberry.jpeg', 3.99, 'fruits', 5);
+VALUES ('Strawberries', 'Fresh strawberries', 7, './docs/strawberry.jpeg', 3.99, 'fruits', 6);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Pineapple', 'Fresh Pineapple', 7, './docs/pineapple.jpeg', 5.99, 'fruits', 5);
+VALUES ('Pineapple', 'Fresh Pineapple', 7, './docs/pineapple.jpeg', 5.99, 'fruits', 6);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Grapefruit', 'Fresh grapefruit', 7, './docs/grapefruit.jpeg', 2.99, 'fruits', 5);
+VALUES ('Grapefruit', 'Fresh grapefruit', 7, './docs/grapefruit.jpeg', 2.99, 'fruits', 7);
 
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Bâtard campagne ', 'Bâtard shaped loaf on a base of rye flour', 10, './docs/batard.jpg', 4.50, 'bread', 4);
@@ -52,13 +52,13 @@ VALUES ('Almond Croissant', 'Almond cream frangipane and sliced brown almonds', 
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Blueberry Pie', 'Fresh blueberry pie with butter crust', 7, './docs/blueberrypie.jpg', 22.99, 'desserts', 2);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Seasonal Pie Duo', 'Flavours: Quebec Apple, cherry, sugar pie, pecan or pear ', 4, './docs/pieduo.jpg', 49.99, 'desserts', 2);
+VALUES ('Seasonal Pie Duo', 'Flavours: Quebec Apple, cherry, sugar pie, pecan or pear ', 4, './docs/pieduo.jpg', 49.99, 'desserts', 5);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Lemon Tart', 'A sweet dough shell with a baked lemon filling', 7, './docs/lemonpie.jpg', 18.99, 'desserts', 5);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Assorted Cookies', 'Decorated sugar cookies, shortbread, oatmeal and other varieties', 7, './docs/cookie.jpg', 10.99, 'desserts', 5);
+VALUES ('Assorted Cookies', 'Decorated sugar cookies, shortbread, oatmeal and other varieties', 7, './docs/cookie.jpg', 10.99, 'desserts', 7);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Chocolate Cheesecake', 'New York cheesecake, plain or dipped in pure chocolate dips', 5, './docs/cheesecake.jpg', 36.99, 'desserts', 5);
+VALUES ('Chocolate Cheesecake', 'New York cheesecake, plain or dipped in pure chocolate dips', 5, './docs/cheesecake.jpg', 36.99, 'desserts', 8);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Assorted Cupcakes', 'Get a box of 8 fresh assorted flavor cupcakes', 7, './docs/cupcake.jpg', 22.99, 'desserts', 5);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
@@ -69,11 +69,11 @@ VALUES ('Carrot Cake', 'The best carrot cake in town', 9, './docs/carrotcake.jpg
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Oka Cheese', 'Semi-soft cheese made with pasteurized milk', 9, './docs/oka.png', 14.99, 'cheese', 2);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Brie Paysan', 'Creamy cheese, the Brie Paysan is made with whole milk.', 7, './docs/brie.jpg', 19.99, 'cheese', 2);
+VALUES ('Brie Paysan', 'Creamy cheese, the Brie Paysan is made with whole milk.', 7, './docs/brie.jpg', 19.99, 'cheese', 1);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Blue of Elizabeth', 'Semi-soft, made from thermalized milk. Interior mould', 9, './docs/bluecheese.jpg', 21.99, 'cheese', 2);
+VALUES ('Blue of Elizabeth', 'Semi-soft, made from thermalized milk. Interior mould', 9, './docs/bluecheese.jpg', 21.99, 'cheese', 7);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Buffala', 'Mozzarella made from the milk of domestic water buffalo', 18, './docs/buffala.jpg', 18.99, 'cheese', 1);
+VALUES ('Buffala', 'Mozzarella made from the milk of domestic water buffalo', 18, './docs/buffala.jpg', 18.99, 'cheese', 7);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Pont-Mousseau', 'Unripened soft white cheese. Silky mousse like texture', 10, './docs/pontmousseau.jpg', 12.99, 'cheese', 1);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
@@ -88,7 +88,7 @@ VALUES ('Beef Wagyu', 'Authentic Wagyu filet mignon, 200g', 6, './docs/wagyu.png
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Chicken Breast', 'Boneless, skinless chicken breast', 6, './docs/chicken.gif', 18.99, 'meat', 2);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Smoked Salmon', 'Prepared fresh on site in our smoking room by our chef', 6, './docs/smokesalmon.jpg', 22.99, 'meat', 2);
+VALUES ('Smoked Salmon', 'Prepared fresh on site in our smoking room by our chef', 6, './docs/smokesalmon.jpg', 22.99, 'meat', 1);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Wild Salmon', 'High in essential Omega 3 oil, and low in saturated fats', 6, './docs/salmon.jpg', 19.99, 'meat', 2);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
@@ -97,9 +97,9 @@ VALUES ('Italian Sausages', 'Italian sausage, but with a mild touch', 6, './docs
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Rise Kombucha', 'Bluberry&Maple, Litchi&Jasmin, Kiwi&Pinnaple', 6, './docs/rise.jpg', 4.29, 'drinks', 1);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Mateina 12 Packs', 'Organic Yerba Mate ', 6, './docs/mateina.png', 39.99, 'drinks', 4);
+VALUES ('Mateina 12 Packs', 'Organic Yerba Mate ', 6, './docs/mateina.png', 39.99, 'drinks', 6);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
-VALUES ('Loop Juice', 'Tasty cold-pressed juices out of reject fruit and vegetables ', 6, './docs/loop.png', 4.69, 'drinks', 4);
+VALUES ('Loop Juice', 'Tasty cold-pressed juices out of reject fruit and vegetables ', 6, './docs/loop.png', 4.69, 'drinks', 6);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
 VALUES ('Non-alc. IPA ', 'Non-alcoholic beer that has some panache! Very hoppy and dry ', 6, './docs/ipa.png', 5.99, 'drinks', 2);
 INSERT INTO products (name, description, quantity, image, price, category, store_id)
