06188ac04344a3bcfcfc8571219211a5b3c28b7d
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-27 19:47:29 +0000

    added gif ;

--- backend-express/db/seeds/01_users.sql
+++ backend-express/db/seeds/01_users.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (first_name, last_name, email, password, phone_number, address, city)
-VALUES ('Matt', 'Ford', 'matt@rockit.com', 'test', '514-222-2222', '222 Saint-Laurent Blvd.', 'Montreal)
+VALUES ('Matt', 'Ford', 'matt@rockit.com', 'test', '514-222-2222', '222 Saint-Laurent Blvd.', 'Montreal')
